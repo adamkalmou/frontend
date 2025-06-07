5ed62fccd39abd80629bf5579e38e7a19dd7df42
adamkalmou <76250909+adamkalmou@users.noreply.github.com>
2025-06-07 23:52:45 +0100

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -9,7 +9,7 @@ import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle }
 import { Skeleton } from "@/components/ui/skeleton";
 import { Progress } from "@/components/ui/progress";
 
-const API_URL = "http://34.155.252.211:8080/";
+const API_URL = "/api/";
 
 const Index = () => {
   const [file, setFile] = useState<File | null>(null);
